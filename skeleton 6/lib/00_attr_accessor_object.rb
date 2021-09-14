77f7ea67b6bff817462ef494731c8e65337cba94
mrossant <matteorossant@gmail.com>
2021-09-14 15:39:45 -0400

    Finished attr_accessor

--- skeleton 6/lib/00_attr_accessor_object.rb
+++ skeleton 6/lib/00_attr_accessor_object.rb
@@ -1,5 +1,16 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    # it should define a getter
+    # it should define a setter
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+    end
   end
 end
